cf096f1dd75cafe80a4c0b5ceafc7bec6a70a50e
TheOpti <wieckowski.piotr.93@gmail.com>
2024-06-06 20:30:40 +0200

    Add validation to LoginForm

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { useMutation } from '@apollo/client';
 
 import { LOGIN_MUTATION } from './LoginModal.graphql';
+import { EMAIL_REGEX } from './LoginModal.utils';
 
 import classes from './LoginModal.module.css';
 
@@ -19,19 +21,35 @@ export const LoginModal = (props: Props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const [login, { data, loading, error }] = useMutation(LOGIN_MUTATION);
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const [login, { loading }] = useMutation(LOGIN_MUTATION);
 
   const doLogin = () => {
+    if (password === '') {
+      setPasswordError('You need to type your password.');
+      return;
+    }
+
+    if (!email.match(EMAIL_REGEX)) {
+      setEmailError('Please provide correct email.');
+      return;
+    }
+
+    setPasswordError('');
+    setEmailError('');
+
     login({
       variables: {
         email,
         password
       },
       onCompleted: () => {
-        console.log('login - onCompleted');
+        console.log('!!! login - onCompleted');
       },
-      onError: () => {
-        console.log('login - onError');
+      onError: (error) => {
+        setEmailError(error.message);
       }
     });
   };
@@ -41,37 +59,53 @@ export const LoginModal = (props: Props) => {
       <Modal.Header closeButton>
         <Modal.Title>Already a user? Sign in!</Modal.Title>
       </Modal.Header>
+
       <Modal.Body>
         <Form className={classes.form}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <InputGroup hasValidation>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                isInvalid={Boolean(emailError)}
+                disabled={loading}
+              />
+              <Form.Control.Feedback type="invalid">{emailError}</Form.Control.Feedback>
+            </InputGroup>
           </Form.Group>
+
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <InputGroup hasValidation>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                isInvalid={Boolean(passwordError)}
+                disabled={loading}
+              />
+              <Form.Control.Feedback type="invalid">{passwordError}</Form.Control.Feedback>
+            </InputGroup>
           </Form.Group>
         </Form>
+
         <div className="d-grid gap-2">
-          <Button variant="primary" onClick={doLogin}>
-            Login
+          <Button variant="primary" onClick={doLogin} disabled={loading}>
+            {loading ? 'Logging in...' : 'Login'}
           </Button>
-          <Button variant="link">Forgot your password?</Button>
+          <Button variant="link" disabled={loading}>
+            Forgot your password?
+          </Button>
         </div>
       </Modal.Body>
+
       <Modal.Footer className={classes.footer}>
         <div className="d-grid gap-2">
-          <Button variant="success" type="submit">
+          <Button variant="success" type="submit" disabled={loading}>
             Create new account
           </Button>
         </div>
